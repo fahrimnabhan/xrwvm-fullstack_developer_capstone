53f05a018fbe98ef0734ed76bf84d08aeaef5daa
fahrimnabhan <62969514+fahrimnabhan@users.noreply.github.com>
2024-05-23 16:38:03 +0900

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,5 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.http import JsonResponse
@@ -42,8 +41,6 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-
-
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -51,7 +48,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    
+
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -59,8 +56,6 @@ def registration(request):
     except Exception:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
-
-    
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
@@ -79,16 +74,16 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
-
-
+    return JsonResponse({
+        "status": 200,
+        "dealers": dealerships
+    })
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
@@ -99,19 +94,29 @@ def get_dealer_reviews(request, dealer_id):
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
+        return JsonResponse({
+            "status": 200,
+            "reviews": reviews
+        })
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
 
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
+        return JsonResponse({
+            "status": 200,
+            "dealer": dealership
+        })
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
 
 def add_review(request):
     if(request.user.is_anonymous == False):
@@ -119,10 +124,16 @@ def add_review(request):
         try:
             response = post_review(data)
             return JsonResponse({"status":200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+        except Exception:
+            return JsonResponse({
+                "status": 401,
+                "message": "Error in posting review"
+            })
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({
+            "status": 403,
+            "message": "Unauthorized"
+        })
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
@@ -132,6 +143,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
     return JsonResponse({"CarModels":cars})
-
